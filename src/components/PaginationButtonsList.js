8fe899cae389b528ca2ba94700b204704df8d507
tushar <tusharkanti647@gmail.com>
2023-06-01 15:23:36 +0000

    page work

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
-const PaginationButtonsList = ({setPageNum}) => {
-    const [isActive, setIsActive] = useState(1);
+const PaginationButtonsList = ({ setPageNum, pageNum }) => {
+    //const [isActive, setIsActive] = useState(0);
 
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
     const handelClick = (ele) => {
         //     e.target.className="active-btn"
-         //console.log(e.target.key)
-         setPageNum(ele);
-        setIsActive(ele);
+        //console.log(e.target.key)
+        setPageNum(ele);
+        //setIsActive(ele);
 
         //e.currentTarget.classList.toggle("active-btn")
     }
 
     return (
         <div className="pagination-buttons-list">
-            {arr.map((ele) => <button className={isActive===ele ?"active-btn" : ""}  onClick={()=>handelClick(ele)} id={"button-" + ele} key={ele}>{ele}</button>)}
+            {arr.map((ele) => <button className={pageNum === ele ? "active-btn" : ""} onClick={() => handelClick(ele)} id={"button-" + ele} key={ele}>{ele}</button>)}
         </div>
     )
 }
