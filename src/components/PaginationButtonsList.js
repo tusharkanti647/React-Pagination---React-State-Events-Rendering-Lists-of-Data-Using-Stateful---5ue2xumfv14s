0e64c3be1f13ab38ace43588bcedf55470b40778
tushar <tusharkanti647@gmail.com>
2023-06-01 13:29:59 +0000

    done

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,9 +1,21 @@
-import React from 'react'
-const PaginationButtonsList = () => {
+import React, { useState } from 'react'
+const PaginationButtonsList = ({setPageNum}) => {
+    const [isActive, setIsActive] = useState(0);
+
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+    const handelClick = (ele) => {
+        //     e.target.className="active-btn"
+         //console.log(e.target.key)
+         setPageNum(ele);
+        setIsActive(ele);
+
+        //e.currentTarget.classList.toggle("active-btn")
+    }
 
     return (
         <div className="pagination-buttons-list">
-
+            {arr.map((ele) => <button className={isActive===ele ?"active-btn" : ""}  onClick={()=>handelClick(ele)} id={"button-" + ele} key={ele}>{ele}</button>)}
         </div>
     )
 }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { PaginationButtonsList } from './PaginationButtonsList'
+import { fetchPosts } from '../api/fetchPosts';
+import { Post } from './Post';
 
 
 const PostList = () => {
+    const [pageNum, setPageNum]=useState(1);
+    const [postData, setPostdata]=useState([]);
     
+useEffect(()=>{
+    const fechFun=async ()=>{
+        const data = await fetchPosts(pageNum);
+        setPostdata([...data]);
+    }
+    fechFun();
+},[pageNum]);
+
+
     return (
         <>
-
+{postData.map((ele, ind)=><Post ind={ind} ele={ele}/>)}
+<PaginationButtonsList setPageNum={setPageNum}/>
         </>
     )
 }
